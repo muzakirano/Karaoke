f3cd5d49d7298951607f43ef3bf2ac7798a0f13f
milegar95 <milegar95@gmail.com>
2014-06-10 14:59:17 -0500

    Atributos clases: autor, genero, cancion, karaoke implementacion hilo clase karaoke

--- Karaoke/src/logica/Autor.java
+++ Karaoke/src/logica/Autor.java
@@ -1,5 +1,28 @@
 package logica;
 
-public class Autor {
+import java.util.ArrayList;
 
+public class Autor {
+	String nombre;
+	private ArrayList<String>listaCanciones= new ArrayList<String>();
+	String fotoAutor;
+	
+	public Autor() {
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getFotoAutor() {
+		return fotoAutor;
+	}
+
+	public void setFotoAutor(String fotoAutor) {
+		this.fotoAutor = fotoAutor;
+	}
 }
--- Karaoke/src/logica/Karaoke.java
+++ Karaoke/src/logica/Karaoke.java
@@ -1,5 +1,44 @@
 package logica;
 
-public class Karaoke {
+import java.util.ArrayList;
 
+public class Karaoke extends Thread{
+	
+	private Cancion cancionActual;
+	private boolean ejecucion;
+	private boolean pausa;
+	private ArrayList<String>listaGeneros= new ArrayList<String>();
+	
+	public Karaoke() {
+
+	}
+			
+	public void iniciar(){
+		ejecucion= true;
+		start();		
+	}
+	
+	public void detener() {
+		ejecucion= false;
+	}
+	
+	public void pausar(){
+		pausa=true;
+	}
+	
+	public void reanudar(){
+		pausa=false;
+	}
+	
+	@Override
+	public void run() {
+		
+		while(ejecucion){
+			
+			while(pausa){
+				System.out.println("nada");
+			}
+		}
+	}
+	
 }
